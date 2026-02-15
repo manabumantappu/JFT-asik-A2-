cd9f31b5c0473090520ad65818858947a145b8a6
manabumantappu <azishachigo@gmail.com>
2026-02-16 06:37:22 +0700

    Update quizService.js

--- js/services/quizService.js
+++ js/services/quizService.js
@@ -2,28 +2,35 @@ import { db } from "../firebase.js";
 import {
   collection,
   query,
+  where,
   orderBy,
+  startAt,
   limit,
-  getDocs,
-  where
+  getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 export async function getQuestionsRandom(limitCount = 20) {
 
   const randomValue = Math.random();
 
+  // Query utama
   const q = query(
     collection(db, "questions"),
     where("type", "==", "quiz"),
     orderBy("random"),
+    startAt(randomValue),
     limit(limitCount)
   );
 
   const snapshot = await getDocs(q);
-  let questions = snapshot.docs.map(doc => doc.data());
+  let questions = snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  }));
 
-  // Jika kurang dari 20 (misalnya random di akhir range)
+  // Jika data kurang dari 20, ambil dari awal
   if (questions.length < limitCount) {
+
     const q2 = query(
       collection(db, "questions"),
       where("type", "==", "quiz"),
@@ -32,8 +39,12 @@ export async function getQuestionsRandom(limitCount = 20) {
     );
 
     const snapshot2 = await getDocs(q2);
+
     questions = questions.concat(
-      snapshot2.docs.map(doc => doc.data())
+      snapshot2.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }))
     );
   }
 
