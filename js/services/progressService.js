b1fa9daba8e9c0c29d25aed266204181fe28c4a9
manabumantappu <azishachigo@gmail.com>
2026-02-16 08:43:43 +0700

    Update progressService.js

--- js/services/progressService.js
+++ js/services/progressService.js
@@ -1,11 +1,36 @@
 import { db, auth } from "../firebase.js";
-import { doc, setDoc, serverTimestamp } from
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import {
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
-export async function saveProgress(score) {
+export async function updateProgress(score) {
+
   const uid = auth.currentUser.uid;
-  await setDoc(doc(db, "progress", uid), {
-    lastScore: score,
-    updatedAt: serverTimestamp()
-  });
+  const progressRef = doc(db, "progress", uid);
+  const snap = await getDoc(progressRef);
+
+  if (!snap.exists()) {
+
+    await setDoc(progressRef, {
+      lastScore: score,
+      totalQuiz: 1,
+      bestScore: score,
+      updatedAt: serverTimestamp()
+    });
+
+  } else {
+
+    const data = snap.data();
+
+    await updateDoc(progressRef, {
+      lastScore: score,
+      totalQuiz: data.totalQuiz + 1,
+      bestScore: Math.max(data.bestScore, score),
+      updatedAt: serverTimestamp()
+    });
+  }
 }
