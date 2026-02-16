a93c32b8f3bc31369c6cc03a4d36dbeb97af763a
manabumantappu <azishachigo@gmail.com>
2026-02-16 15:12:37 +0700

    Update katakana.js

--- js/data/katakana.js
+++ js/data/katakana.js
@@ -1,13 +1,172 @@
 export const katakanaData = [
+
+  // ================= VOKAL =================
   { char: "ア", romaji: "a" },
   { char: "イ", romaji: "i" },
   { char: "ウ", romaji: "u" },
   { char: "エ", romaji: "e" },
   { char: "オ", romaji: "o" },
 
+  // ================= K =================
   { char: "カ", romaji: "ka" },
   { char: "キ", romaji: "ki" },
   { char: "ク", romaji: "ku" },
   { char: "ケ", romaji: "ke" },
-  { char: "コ", romaji: "ko" }
+  { char: "コ", romaji: "ko" },
+
+  // ================= S =================
+  { char: "サ", romaji: "sa" },
+  { char: "シ", romaji: "shi" },
+  { char: "ス", romaji: "su" },
+  { char: "セ", romaji: "se" },
+  { char: "ソ", romaji: "so" },
+
+  // ================= T =================
+  { char: "タ", romaji: "ta" },
+  { char: "チ", romaji: "chi" },
+  { char: "ツ", romaji: "tsu" },
+  { char: "テ", romaji: "te" },
+  { char: "ト", romaji: "to" },
+
+  // ================= N =================
+  { char: "ナ", romaji: "na" },
+  { char: "ニ", romaji: "ni" },
+  { char: "ヌ", romaji: "nu" },
+  { char: "ネ", romaji: "ne" },
+  { char: "ノ", romaji: "no" },
+
+  // ================= H =================
+  { char: "ハ", romaji: "ha" },
+  { char: "ヒ", romaji: "hi" },
+  { char: "フ", romaji: "fu" },
+  { char: "ヘ", romaji: "he" },
+  { char: "ホ", romaji: "ho" },
+
+  // ================= M =================
+  { char: "マ", romaji: "ma" },
+  { char: "ミ", romaji: "mi" },
+  { char: "ム", romaji: "mu" },
+  { char: "メ", romaji: "me" },
+  { char: "モ", romaji: "mo" },
+
+  // ================= Y =================
+  { char: "ヤ", romaji: "ya" },
+  { char: "ユ", romaji: "yu" },
+  { char: "ヨ", romaji: "yo" },
+
+  // ================= R =================
+  { char: "ラ", romaji: "ra" },
+  { char: "リ", romaji: "ri" },
+  { char: "ル", romaji: "ru" },
+  { char: "レ", romaji: "re" },
+  { char: "ロ", romaji: "ro" },
+
+  // ================= W =================
+  { char: "ワ", romaji: "wa" },
+  { char: "ヲ", romaji: "wo" },
+
+  // ================= N =================
+  { char: "ン", romaji: "n" },
+
+  // =================================================
+  // =============== DAKUTEN =========================
+  // =================================================
+
+  { char: "ガ", romaji: "ga" },
+  { char: "ギ", romaji: "gi" },
+  { char: "グ", romaji: "gu" },
+  { char: "ゲ", romaji: "ge" },
+  { char: "ゴ", romaji: "go" },
+
+  { char: "ザ", romaji: "za" },
+  { char: "ジ", romaji: "ji" },
+  { char: "ズ", romaji: "zu" },
+  { char: "ゼ", romaji: "ze" },
+  { char: "ゾ", romaji: "zo" },
+
+  { char: "ダ", romaji: "da" },
+  { char: "ヂ", romaji: "ji" },
+  { char: "ヅ", romaji: "zu" },
+  { char: "デ", romaji: "de" },
+  { char: "ド", romaji: "do" },
+
+  { char: "バ", romaji: "ba" },
+  { char: "ビ", romaji: "bi" },
+  { char: "ブ", romaji: "bu" },
+  { char: "ベ", romaji: "be" },
+  { char: "ボ", romaji: "bo" },
+
+  // =================================================
+  // =============== HANDAKUTEN ======================
+  // =================================================
+
+  { char: "パ", romaji: "pa" },
+  { char: "ピ", romaji: "pi" },
+  { char: "プ", romaji: "pu" },
+  { char: "ペ", romaji: "pe" },
+  { char: "ポ", romaji: "po" },
+
+  // =================================================
+  // =============== YOUON ===========================
+  // =================================================
+
+  { char: "キャ", romaji: "kya" },
+  { char: "キュ", romaji: "kyu" },
+  { char: "キョ", romaji: "kyo" },
+
+  { char: "シャ", romaji: "sha" },
+  { char: "シュ", romaji: "shu" },
+  { char: "ショ", romaji: "sho" },
+
+  { char: "チャ", romaji: "cha" },
+  { char: "チュ", romaji: "chu" },
+  { char: "チョ", romaji: "cho" },
+
+  { char: "ニャ", romaji: "nya" },
+  { char: "ニュ", romaji: "nyu" },
+  { char: "ニョ", romaji: "nyo" },
+
+  { char: "ヒャ", romaji: "hya" },
+  { char: "ヒュ", romaji: "hyu" },
+  { char: "ヒョ", romaji: "hyo" },
+
+  { char: "ミャ", romaji: "mya" },
+  { char: "ミュ", romaji: "myu" },
+  { char: "ミョ", romaji: "myo" },
+
+  { char: "リャ", romaji: "rya" },
+  { char: "リュ", romaji: "ryu" },
+  { char: "リョ", romaji: "ryo" },
+
+  { char: "ギャ", romaji: "gya" },
+  { char: "ギュ", romaji: "gyu" },
+  { char: "ギョ", romaji: "gyo" },
+
+  { char: "ジャ", romaji: "ja" },
+  { char: "ジュ", romaji: "ju" },
+  { char: "ジョ", romaji: "jo" },
+
+  { char: "ビャ", romaji: "bya" },
+  { char: "ビュ", romaji: "byu" },
+  { char: "ビョ", romaji: "byo" },
+
+  { char: "ピャ", romaji: "pya" },
+  { char: "ピュ", romaji: "pyu" },
+  { char: "ピョ", romaji: "pyo" },
+
+  // =================================================
+  // =============== FOREIGN SOUND ===================
+  // =================================================
+
+  { char: "ファ", romaji: "fa" },
+  { char: "フィ", romaji: "fi" },
+  { char: "フェ", romaji: "fe" },
+  { char: "フォ", romaji: "fo" },
+
+  { char: "ティ", romaji: "ti" },
+  { char: "ディ", romaji: "di" },
+
+  { char: "トゥ", romaji: "tu" },
+  { char: "ドゥ", romaji: "du" }
+
 ];
