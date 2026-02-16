c69ab7b320dcdc5a228bd2b7a90dfc563a010ea6
manabumantappu <azishachigo@gmail.com>
2026-02-16 09:25:06 +0700

    Update roleService.js

--- js/services/roleService.js
+++ js/services/roleService.js
@@ -1,9 +1,14 @@
 import { db, auth } from "../firebase.js";
-import { doc, getDoc } from 
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { doc, getDoc } from
+"https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
 export async function getUserRole() {
   const uid = auth.currentUser.uid;
   const snap = await getDoc(doc(db, "users", uid));
-  return snap.data()?.role || "user";
+
+  if (snap.exists()) {
+    return snap.data().role;
+  }
+
+  return "user";
 }
