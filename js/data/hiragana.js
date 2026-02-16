860950cc23de0534e1502f715c30f1212e78bcb1
manabumantappu <azishachigo@gmail.com>
2026-02-16 14:58:55 +0700

    Create hiragana.js

---
+++ js/data/hiragana.js
@@ -0,0 +1,19 @@
+export const hiraganaData = [
+  { char: "あ", romaji: "a" },
+  { char: "い", romaji: "i" },
+  { char: "う", romaji: "u" },
+  { char: "え", romaji: "e" },
+  { char: "お", romaji: "o" },
+
+  { char: "か", romaji: "ka" },
+  { char: "き", romaji: "ki" },
+  { char: "く", romaji: "ku" },
+  { char: "け", romaji: "ke" },
+  { char: "こ", romaji: "ko" },
+
+  { char: "さ", romaji: "sa" },
+  { char: "し", romaji: "shi" },
+  { char: "す", romaji: "su" },
+  { char: "せ", romaji: "se" },
+  { char: "そ", romaji: "so" }
+];
